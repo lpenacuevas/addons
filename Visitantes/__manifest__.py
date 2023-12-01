ea7673bd982bee2650e9e17627a63fe883cb3bff
Lorenzo Peña <L.cuevas@mitur.gob.do>
2023-12-01 14:30:51 -0400

    lasted

--- Visitantes/__manifest__.py
+++ Visitantes/__manifest__.py
@@ -17,7 +17,7 @@
     'version': '0.1.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','mail', 'hr'],
+    'depends': ['base','mail', 'hr', 'contacts'],
 
     # always loaded
     'data': [
--- Visitantes/models/__init__.py
+++ Visitantes/models/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 
 from . import visitantes
+from . import tags_visitantes
