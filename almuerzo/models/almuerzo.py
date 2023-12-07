572b8700fb30b9d3bd38ca357465aee7c98ca7ce
Luisbert Aquino <luisbert_aquino@hotmail.com>
2023-12-07 12:57:53 -0400

    Build logic to retrieve information from employees and contacts and change the visibility of datetime fields from_date and date_to

--- almuerzo/models/almuerzo.py
+++ almuerzo/models/almuerzo.py
@@ -1,4 +1,8 @@
+import logging
 from odoo import models, fields, api
+from odoo.exceptions import UserError
+
+_logger = logging.getLogger(__name__)
 
 
 class almuerzo(models.Model):
@@ -8,37 +12,60 @@ class almuerzo(models.Model):
 
     # name = fields.Char("test")
     
-    cedula = fields.Char("Cedula", store=True, compute='_change_fields')
+    cedula = fields.Char("Cedula")
     name = fields.Char("Nombre")
     last_name = fields.Char("Apellidos")
-    quantity = fields.Integer("Cantidad")
+    quantity = fields.Integer("Cantidad", default="1")
     from_date = fields.Date("Fecha desde")
     date_to = fields.Date("Fecha hasta")
+    employee_object = fields.Many2one("hr.employee")
+    contact_object = fields.Many2one("res.partner")
 
 
-#TODO: Validar cedula con maestro de contactos
-#TODO: Validar cedula con maestro de empleados
-#TODO: Mantenimiento tipo de empleado
+    @api.onchange("cedula")
+    def _compute_lunch_parameters(self):
+
+        for rec in self:
+            employee_obj = self.env['hr.employee'].search([('identification_id', '=', rec.cedula)], limit=1)
+            contact_obj = self.env['res.partner'].search([('vat', '=', rec.cedula)], limit=1)
+            
+
+            #employee_id_format = #TODO: FORMAT CEDULA
+            if employee_obj and rec.cedula:
+                rec.name = employee_obj.name
+                rec.last_name = employee_obj.last_name
+                rec.contact_object = False
+                
+            
+            elif contact_obj and rec.cedula:    
+                rec.name = contact_obj.firstname
+                rec.last_name = contact_obj.lastname
+                rec.contact_object = contact_obj.id
+                _logger.info(f"-------------{rec.contact_object.id}------------------")
+
+    
+
+#TODO: Cambiar rec_name, comcapt name,last_name
+#TODO: Crear logica para permitir el almuerzo tomando como parámetros las fechas
+#TODO: Crear alerta de acceso permitido (Verde) o denegado (Roja). Si es denegado debe de lanzar una excepcion.
+#TODO: Pantalla principal --> Nombre,apellido 
+#TODO: Cuando es empleado debe de traer el departamento
+#TODO: Crear campo tipo de persona Many2one relacionado con el mantenimiento de tipo de persona
+#TODO: Crear la vista de lista para el registro de cada persona (Nombre, apellido, departamento, tipo de persona). Nota: Si es personal externo el departamento es el dept administrativo
 
 
-# WORKIN ON THIS
-# @api.onchange("cedula")
-# def _change_fields(self):
-#     # Getting contact object list by cedula
-#     contact_obj = self.env['res.partner'].search([('vat', '=', self.cedula)], limit=1)
+    # THIS Works
+    # @api.onchange("cedula")
+    # def _change_fields(self):
+    #     for rec in self:
+    #         # Getting contact object list by cedula
+    #         contact_obj = self.env['res.partner'].search([('vat', '=', rec.cedula)], limit=1)
+    #         _logger.info(f"--------------ENTRO------------------")
+    #         _logger.info(f"-------------{rec}------------------")
+    #         _logger.info(f"-------------{rec.cedula}------------------")
+    #         _logger.info(f"-------------{rec.name}------------------")
 
-#     # Itering on records in the contact object
-#     for rec in contact_obj:
-#         if rec:
-#             self.name = rec.first_name
-#             self.last_name = rec.last_name
-#             # rec.write({"cedula": self.cedula})
-
-
-
-# def change_visitor_status(self):
-#     for rec in self:
-#         record = rec.env['visitante'].browse(rec._context.get('active_id'))
-#         status_tag = rec.env['tags.visitantes'].search([('name', '=', 'Salida')], limit=1)
-#     if record and status_tag:
-#         record.write({'state': [(6, 0, [status_tag.id])]})
+    #         if contact_obj and rec.cedula:
+    #             rec.name = contact_obj.firstname
+    #             rec.last_name = contact_obj.lastname
+                # raise UserError("User Error")
