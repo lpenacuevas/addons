86e8312230f97f69229f5b23399f0d0de8070e44
Luisbert Aquino <luisbert_aquino@hotmail.com>
2023-12-13 19:45:42 -0400

    Modified _compute_lunch_parameters on almuerzo.py to just work with contacts
    Now the _compute_lunch_parameters function just works with contacts because it is not necessary to grant access to employees
    because they already have access to eat unless they are not longer working to the company and the access for employess is now validated on almuerzo.despacho.almuerzo model.

--- almuerzo/models/almuerzo.py
+++ almuerzo/models/almuerzo.py
@@ -27,19 +27,15 @@ class almuerzo(models.Model):
     def _compute_lunch_parameters(self):
 
         for rec in self:
-            employee_obj = self.env['hr.employee'].search([('identification_id', '=', rec.cedula)], limit=1)
-            contact_obj = self.env['res.partner'].search([('vat', '=', rec.cedula)], limit=1)
+            if not rec.cedula:
+                return
             
+            formatted_cedula = rec.cedula.replace('-', '')
+            rec.cedula = formatted_cedula
 
-            #employee_id_format = #TODO: FORMAT CEDULA
-            if employee_obj and rec.cedula:
-                rec.first_name = employee_obj.name
-                rec.last_name = employee_obj.last_name
-                rec.contact_object = False
-                rec.unit = employee_obj.department_id.name
-
+            contact_obj = self.env['res.partner'].search([('vat', '=', formatted_cedula), ('vat', '!=', '')], limit=1)
             
-            elif contact_obj and rec.cedula:    
+            if contact_obj.name:
                 rec.first_name = contact_obj.firstname
                 rec.last_name = contact_obj.lastname
                 rec.contact_object = contact_obj.id
@@ -54,24 +50,5 @@ class almuerzo(models.Model):
             return rec.name
 
 
-#TODO: Crear logica para permitir el almuerzo tomando como parámetros las fechas
-#TODO: Crear alerta de acceso permitido (Verde) o denegado (Roja). Si es denegado debe de lanzar una excepcion.
-#TODO: Pantalla principal --> Nombre,apellido 
-#TODO: Crear la vista de lista para el registro de cada persona (Nombre, apellido, departamento, tipo de persona). Nota: Si es personal externo el departamento es el dept administrativo
-
-
-    # THIS Works
-    # @api.onchange("cedula")
-    # def _change_fields(self):
-    #     for rec in self:
-    #         # Getting contact object list by cedula
-    #         contact_obj = self.env['res.partner'].search([('vat', '=', rec.cedula)], limit=1)
-    #         _logger.info(f"--------------ENTRO------------------")
-    #         _logger.info(f"-------------{rec}------------------")
-    #         _logger.info(f"-------------{rec.cedula}------------------")
-    #         _logger.info(f"-------------{rec.name}------------------")
-
-    #         if contact_obj and rec.cedula:
-    #             rec.name = contact_obj.firstname
-    #             rec.last_name = contact_obj.lastname
-                # raise UserError("User Error")
+#TODO: Modificar nombre del modelo a almuerzo.administracion y la descripción a Administración de almuerzo
+#TODO: Establecer los campos requeridos
