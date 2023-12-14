00ebc6c127d862f8e7cee037d281403e491f1f07
Luisbert Aquino <luisbert_aquino@hotmail.com>
2023-12-14 09:37:16 -0400

    Modified the functionality of almuerzo.despacho.almuerzo model
    The description of the module has been changed to be more accurate regarding the function of the model.
    The 'cedula' field now has an accent mark.
    The 'name' and 'unit' fields are now readonly.
    Compute and store attributes were removed from the 'name' field because it is dynamically calculated with the compute_full_name function.
    The 'employee_object' field was removed.
    The 'contact_object' now points to the almuerzo.almuerzo model.
    
    The function 'compute_lunch_parameters' has been renamed to 'update_lunch_parameters' for better alignment with the added functionality.
    'compute_lunch_parameters' now has a docstring that explains its functionality.
    
    A validation was added to handle the case when a 'cedula' is not provided.
    Remove dashes from 'cedula' or identification number provided by the user.
    Handle cases based on whether the person is an employee or contact allowed to eat.
    
    'compute_rec_name' was renamed to 'compute_full_name' to better match its functionality.
    
    The 'send_notification' function was commented out because it is still in development.

--- almuerzo/models/despacho_almuerzo.py
+++ almuerzo/models/despacho_almuerzo.py
@@ -1,57 +1,119 @@
-import datetime
+from datetime import datetime
 import logging
 from odoo import models, fields, api
-from odoo.exceptions import UserError
+from odoo.exceptions import ValidationError
 
 _logger = logging.getLogger(__name__)
 
 
 class almuerzo_comida(models.Model):
     _name = 'almuerzo.despacho.almuerzo'
-    _description = 'Modelo de registro de almuerzo diario'
+    _description = 'Modelo de despacho de almuerzo'
     
-    cedula = fields.Char("Cedula")
+    cedula = fields.Char("Cédula")
     first_name = fields.Char("Nombre")
     last_name = fields.Char("Apellidos")
-    name = fields.Char("Nombre completo", store=True, compute="_compute_rec_name")
-    unit = fields.Char("Unidad")
-    employee_object = fields.Many2one("hr.employee")
-    contact_object = fields.Many2one("res.partner")
-    #allowed_to_eat = fields.Boolean(string='Puede comer', default=True)
+    name = fields.Char("Nombre completo", readonly=True)
+    unit = fields.Char("Unidad", readonly=True)
+    tipo_persona_almuerza = fields.Many2one("almuerzo.almuerzo")
+    contact_object = fields.Many2one("almuerzo.almuerzo")
 
     @api.onchange("cedula")
-    def _compute_lunch_parameters(self):
+    def update_lunch_parameters(self):
 
         for rec in self:
-            employee_obj = self.env['hr.employee'].search([('identification_id', '=', rec.cedula)], limit=1)
-            contact_obj = self.env['res.partner'].search([('vat', '=', rec.cedula)], limit=1)
+            """
+            Update lunch parameters based on the identification number provided.
 
-            #employee_id_format = #TODO: FORMAT CEDULA
-            if employee_obj and rec.cedula:
-                rec.first_name = employee_obj.name
-                rec.last_name = employee_obj.last_name
-                rec.unit = employee_obj.department_id.name
+            This method is triggered whenever the identification number field is changed. It removes dashes from
+            the identification number, searches for an employee and the most recent lunch record associated with the
+            identification number, and updates the lunch parameters accordingly.
 
-            elif contact_obj and rec.cedula:    
-                rec.first_name = contact_obj.firstname
-                rec.last_name = contact_obj.lastname
-                rec.contact_object = contact_obj.id
-                rec.unit = "VICEMINISTERIO ADMINISTRATIVO"
+            If an employee is allowed to eat, the method sets the first name, last name, unit, and triggers the
+            notification for successful access. If the identification number does not match any records or the person
+            is not allowed to eat, a warning is logged, and a validation error is raised.
 
-    #Working on this
-    # def is_allowed_to_eat(self):
-    #     """Grant access to the collaborator to be able to eat"""
-    #     if self.allowed_to_eat:
-    #         if self.employee_object:
-    #             return self.employee_object.active and (datetime.today() >= self.employee_object.enter_date and datetime.today() <= self.employee_object.end_date)
-    #         else:
-    #             return self.from_date <= datetime.today() <= self.date_to
-    #     else:
-    #         return False
+            If the person is a contact allowed to eat, the method sets the first name, last name, contact object,
+            unit, and triggers the notification for successful access.
+
+            :return: None
+            :raises: ValidationError if the identification number is not found or the person is not allowed to eat.
+            """
+
+            if not rec.cedula:
+                return
+
+            # Remove dashes from the identification number
+            formatted_cedula = rec.cedula.replace('-', '')
+            rec.cedula = formatted_cedula
+
+            # Search for an employee and the most recent lunch record associated with the identification number
+            employee_obj = self.env['hr.employee'].search([('identification_id', '=', formatted_cedula), ('identification_id', '!=', '')], limit=1)
+            contact_records = self.env['almuerzo.almuerzo'].search([('cedula', '=', formatted_cedula)])
+            contact_obj = contact_records.sorted(key=lambda r: r.create_date, reverse=True)[0] if contact_records else None
+            
+            if formatted_cedula:
+                # Check if the employee is allowed to eat
+                employee_is_allowed_to_eat = True if employee_obj and employee_obj.enter_date and ((not employee_obj.end_date) or (employee_obj.end_date and datetime.now().date() <= employee_obj.end_date)) else False
+                contact_is_allowed_to_eat = True if contact_obj and datetime.now().date() >= contact_obj.from_date and datetime.now().date() <= contact_obj.date_to else False
+
+                # Handle cases based on whether the person is an employee or contact allowed to eat
+                if not (employee_is_allowed_to_eat or contact_is_allowed_to_eat):
+                    raise ValidationError("Cédula no encontrada. Por favor, ingrese una cédula válida o póngase en contacto con el administrador del sistema.")
+
+                if employee_is_allowed_to_eat:
+                    rec.first_name = employee_obj.name
+                    rec.last_name = employee_obj.last_name
+                    rec.unit = employee_obj.department_id.name
+                    self._compute_rec_name()
+                elif contact_is_allowed_to_eat:
+                    rec.first_name = contact_obj.first_name
+                    rec.last_name = contact_obj.last_name
+                    rec.contact_object = contact_obj.id
+                    rec.unit = self.env['hr.department'].search([('id_mrh', '=', '37')]).name
+                    self._compute_rec_name()
+
 
     @api.depends('first_name', 'last_name')
-    def _compute_rec_name(self):
-        """Set rec_name as a full name throughout the name field"""
+    def _compute_full_name(self):
+        """Set full name throughout the name field"""
         for rec in self:
             rec.name = f"{self.first_name} {self.last_name}"
             return rec.name
+
+    # Working on this
+    # def send_notification(self, notification_type):
+    #     """ Send a non-sticky notification that disappears from the screen a few seconds after being triggered
+    #         based on the provided notification type.
+
+    #         The method checks whether the notification type is 'success' or 'danger' and constructs a notification
+    #         with a title and message accordingly. Success notifications have a default title of 'Access Allowed',
+    #         while danger notifications have a title of 'Access Denied'. The notifications are of non-sticky type,
+    #         meaning they disappear from the screen after a short duration.
+
+    #         :param notification_type: A string indicating the type of notification, either 'success' or 'danger'.
+    #         :return: A dictionary representing the notification configuration.
+    #     """
+    #     if notification_type not in ['success', 'danger']:
+    #         raise ValueError("Invalid notification type. Supported types: 'success', 'danger'")
+
+    #     notification = {
+    #         'type': 'ir.actions.client',
+    #         'tag': 'display_notification',
+    #         'params': {
+    #             'title': ('ACCESO PERMITIDO'),
+    #             'message': 'ACCESO PERMITIDO',
+    #             'type':'success',  #types: success,warning,danger,info
+    #             'sticky': True,  #True/False will display for few seconds if false
+    #         },
+    #     }
+
+    #     if notification_type == 'success':
+    #         return notification
+    #     elif notification_type == 'danger':
+    #         notification['params']['title'] = 'ACCESO DENEGADO'
+    #         notification['params']['message'] = 'DENEGADO'
+    #         notification['params']['type'] = notification_type
+    #         return notification
+
+#TODO: Crear alerta de acceso permitido (Verde) o denegado (Roja). Si es denegado debe de lanzar una excepcion.
